33a294f2cb4f679e9539894d145f1da24304f51e
FarahAwad <78408663+FarahAwad03@users.noreply.github.com>
2022-07-04 03:18:58 +0200

    Branch and Register cpp file (16 & 32)

---
+++ RB.cpp
@@ -0,0 +1,119 @@
+#include <iostream>
+#include <vector>
+#include <bitset>
+#include "RB.h"
+
+using namespace std;
+
+
+string ABI[32] = { "zero", "ra", "sp", "gp", "tp", "t0","t1", "t2", "s0", "s1", "a0", "a1", "a2", "a3" ,"a4", "a5", "a6", "a7", "s2", "s3", "s4", "s5", "s6", "s7", "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6" };
+string CABI[8] = { "s0", "s1", "a0", "a1",  "a2", "a3", "a4", "a5" };
+
+
+
+void IW32(unsigned int IW);
+void IW16(unsigned int IW);
+
+
+int main()
+{
+	//Testing 32 bits IW:
+	/*unsigned int bin = 0xC6520F23;
+	unsigned int lhu = 0x00545403;
+	unsigned int test2 = 0x40535293;
+	InstDec32bit(lhu);*/
+	//unsigned int add_test = 0x90A2;
+	
+
+
+	system("pause");
+	return 0;
+}
+
+void IW32(unsigned int IW)
+{
+	unsigned int opcode, rd, funct3, rs1, rs2, funct7;
+	unsigned int immediate_12, immediate_10to5, immediate_4to1, immediate_11;
+
+	opcode = IW & 0x7F; //Last 7 bits
+	funct3 = (IW >> 12) & 0x7; //3 bits
+	rs1 = (IW >> 15) & 0x1F; //5 bits
+	rs2 = (IW >> 20) & 0x1F; //5 bits
+	rd = (IW >> 7) & 0x1F; //5 bits
+	funct7 = (IW >> 25) & 0x7F; // 7 bits
+	immediate_12 = (IW >> 31) & 0x1; //1 bit
+	immediate_10to5 = (IW >> 25) & 0x3F; //6 bits
+	immediate_4to1 = (IW >> 8) & 0x1F; //4 bits
+	immediate_11 = (IW >> 7) & 0x1; //1 bit
+
+	cout << "Test: " << opcode << " " << funct3 << " " << rs1 << " " << rs2 << " " << immediate_12 << endl;
+	switch (opcode)
+	{
+	case (51):
+	{
+		if (funct7 == 0)
+		{
+			cout << Register32[funct3] << " " << ABI[rd] << ", " << ABI[rs2] << ABI[rs2] << "\n";
+		}
+		else
+		{
+			if (funct3 == 0)
+			{
+				cout << Register32[8] << " " << ABI[rd] << ", " << ABI[rs2] << ABI[rs2] << "\n";
+			}
+
+			if (funct3 == 5)
+			{
+				cout << Register32[9] << " " << ABI[rd] << ", " << ABI[rs2] << ABI[rs2] << "\n";
+			}
+		}
+		break;
+	}
+	case (99):
+	{
+		string immediate;
+		immediate = to_string(immediate_11) + to_string(immediate_10to5) + to_string(immediate_4to1) + to_string(0);
+		string bin(immediate);
+		int temp = stoi(bin, nullptr, 2);
+
+		if (immediate_12 == '1')
+			temp = 0 - temp;
+
+		cout << Branch32[funct3] << " " << ABI[rs1] << ", " << ABI[rs2] << ", " << temp << endl;
+		break;
+	}
+
+	default:
+		cout << "Unkown Instruction \n";
+	}
+
+}
+
+void IW16(unsigned int IW)
+{
+	unsigned int opcode, ADD_rs2, ADD_rd_rs1, funct4, A_rs2, funct2, A_rd_rs1, funct6;
+
+	opcode = IW & 0x3; //Last 2 bits
+	ADD_rs2 = (IW >> 2) & 0x1F; //5 bits
+	ADD_rd_rs1 = (IW >> 7) & 0x1F; //5 bits
+	funct4 = (IW >> 12) & 0xF; // 4 bits
+
+	A_rs2 = (IW >> 2) & 0x7; // 3 bits
+	funct2 = (IW >> 5) & 0x3; // 2 bits
+	A_rd_rs1 = (IW >> 7) & 0x7; //3 bits
+	funct6 = (IW >> 10) & 0x3F; // 6 bits
+
+	cout << "opcode: " << opcode << endl;
+	cout << ADD_rs2 << " " << ADD_rd_rs1 << endl;
+
+	if (opcode == 1 && funct6 == 35)
+		cout << Arithmetic16[funct2] << " " << CABI[A_rd_rs1] << ", " << CABI[A_rd_rs1] << ", " << CABI[A_rs2] << endl;
+	else if (opcode == 2 && funct4 == 9 && ADD_rs2 != 0 && ADD_rd_rs1 != 0)
+		cout << "ADD " << CABI[ADD_rd_rs1] << ", " << CABI[ADD_rd_rs1] << ", " << CABI[ADD_rs2] << endl;
+	else
+		cout << "Unkown instruction \n";
+}
+
+
+
+
